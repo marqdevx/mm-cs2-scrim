44a2e8a1001f906cb6b1844b499b68534f44cbca
marqdevx <marqdevx@users.noreply.github.com>
2023-10-16 02:51:36 +0200

    Add commands

--- src/adminsystem.cpp
+++ src/adminsystem.cpp
@@ -26,6 +26,9 @@
 #include "playermanager.h"
 #include "commands.h"
 #include <ctime>
+#include <stdio.h>
+#include <string>
+#include "filesystem.h"
 
 extern IVEngineServer2 *g_pEngineServer2;
 extern CEntitySystem *g_pEntitySystem;
@@ -48,7 +51,7 @@ CON_COMMAND_F(c_reload_admins, "Reload admin config", FCVAR_SPONLY | FCVAR_LINKE
 		pPlayer->CheckAdmin();
 	}
 }
-
+/*
 CON_COMMAND_F(c_reload_infractions, "Reload admin config", FCVAR_SPONLY | FCVAR_LINKED_CONCOMMAND)
 {
 	g_pAdminSystem->LoadInfractions();
@@ -64,6 +67,198 @@ CON_COMMAND_F(c_reload_infractions, "Reload admin config", FCVAR_SPONLY | FCVAR_
 	}
 
 	Message("Infractions reloaded\n");
+}*/
+
+CON_COMMAND_CHAT(scrim, "Scrim mode")
+{
+	if (!player)
+		return;
+
+	int iCommandPlayer = player->GetPlayerSlot();
+
+	ZEPlayer *pPlayer = g_playerManager->GetPlayer(player->GetPlayerSlot());
+
+	if (!pPlayer->IsAdminFlagSet(ADMFLAG_SLAY))
+	{
+		ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIX "You don't have access to this command.");
+		return;
+	}
+
+	
+
+	char buf[256];
+	//V_snprintf(buf, sizeof(buf), "exec %s", args[1]);
+
+	g_pEngineServer2->ServerCommand("exec scrim");
+
+	ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIX"Scrim mode loaded, mr24");
+	ClientPrintAll(HUD_PRINTTALK, CHAT_PREFIX"Available commands: \4.pause\1, \4.unpause\1");
+	//ClientPrintAll(HUD_PRINTTALK, CHAT_PREFIX"Admin commands: \4.pracc\1, \4.scrim\1, \4.record\1, \4.stoprecord\1, \4.map\1");
+}
+
+CON_COMMAND_CHAT(pracc, "Practice mode")
+{
+	if (!player)
+		return;
+	
+	int iCommandPlayer = player->GetPlayerSlot();
+
+	ZEPlayer *pPlayer = g_playerManager->GetPlayer(player->GetPlayerSlot());
+
+	if (!pPlayer->IsAdminFlagSet(ADMFLAG_SLAY))
+	{
+		ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIX "You don't have access to this command.");
+		return;
+	}
+
+	char buf[256];
+	//V_snprintf(buf, sizeof(buf), "exec %s", args[1]);
+
+	g_pEngineServer2->ServerCommand("exec pracc");
+
+	ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIX"Practice mode loaded");
+	ClientPrintAll(HUD_PRINTTALK, CHAT_PREFIX"Available commands: \4.pause\1, \4.unpause\1");
+	//ClientPrintAll(HUD_PRINTTALK, CHAT_PREFIX"Admin commands: \4.pracc\1, \4.scrim\1, \4.record\1, \4.stoprecord\1, \4.map\1");
+}
+
+CON_COMMAND_CHAT(forceunpause, "Force unpause")
+{
+	if (!player)
+		return;
+
+	int iCommandPlayer = player->GetPlayerSlot();
+
+	ZEPlayer *pPlayer = g_playerManager->GetPlayer(player->GetPlayerSlot());
+
+	if (!pPlayer->IsAdminFlagSet(ADMFLAG_SLAY))
+	{
+		ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIX "You don't have access to this command.");
+		return;
+	}
+
+	ClientPrintAll(HUD_PRINTTALK, CHAT_PREFIX"Forced unpause");
+
+	char buf[256];
+	//V_snprintf(buf, sizeof(buf), "exec %s", args[1]);
+
+	g_pEngineServer2->ServerCommand("mp_unpause_match");
+}
+
+CON_COMMAND_CHAT(restore, "Restore round")
+{
+	if (!player)
+		return;
+
+	int iCommandPlayer = player->GetPlayerSlot();
+
+	ZEPlayer *pPlayer = g_playerManager->GetPlayer(player->GetPlayerSlot());
+
+	if (!pPlayer->IsAdminFlagSet(ADMFLAG_SLAY))
+	{
+		ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIX "You don't have access to this command.");
+		return;
+	}
+
+	if (args.ArgC() < 2)
+	{
+		ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIX "Usage: !restore <round number>");
+		return;
+	}
+
+	char buf[256];
+	
+	//Save the round number
+    char nRound[2];
+
+    //Needed to build a string for the default restore, format "backup_round<round>.txt" the <round> with "XX" format like "backup_round02.txt"
+    char backupFile[256] = "backup_round";
+	char aux[256] = "";
+        
+	int nRounds = atoi(args[1]);
+	
+	V_snprintf(aux, MAX_PATH, "%s", backupFile);
+
+	if ( nRounds < 10) V_snprintf(buf, MAX_PATH, "%s%i",aux, 0);
+
+	V_snprintf(aux, MAX_PATH, "%s%i.txt", buf, nRounds);
+
+	V_snprintf(buf, MAX_PATH, "mp_backup_restore_load_file %s", aux);
+
+	ClientPrintAll(HUD_PRINTTALK, CHAT_PREFIX"\4Restored round \3%i", nRounds);
+
+	g_pEngineServer2->ServerCommand(buf);
+}
+
+char demoName[128];
+CON_COMMAND_CHAT(record, "Record demo")
+{
+	if (!player)
+		return;
+
+	int iCommandPlayer = player->GetPlayerSlot();
+
+	ZEPlayer *pPlayer = g_playerManager->GetPlayer(player->GetPlayerSlot());
+
+	if (!pPlayer->IsAdminFlagSet(ADMFLAG_SLAY))
+	{
+		ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIX "You don't have access to this command.");
+		return;
+	}
+
+	char buf[256];
+	char actualTime[32];
+	char actualMap[32];
+
+	std::tm tm{};
+	std::time_t result = std::time(nullptr);
+	std::localtime(&result);
+	std::strftime(actualTime, sizeof(actualTime), "%d%B_%H-%M", std::localtime(&result));
+
+	V_snprintf(actualMap,MAX_PATH, "unknownMap");
+	V_snprintf(demoName, MAX_PATH, "%s_%s", actualTime, actualMap);
+
+	V_snprintf(buf, MAX_PATH, "tv_record gotv/%s", demoName);
+	ClientPrint(player, HUD_PRINTTALK, buf);
+	g_pEngineServer2->ServerCommand(buf);
+	//V_snprintf(buf, MAX_PATH, "record gotv/%s", demoName);
+/*
+	FormatTime(actualTime, sizeof(actualTime), "%d%B_%H-%M", GetTimestamp());	//https://www.tutorialspoint.com/c_standard_library/c_function_strftime.htm
+	GetCurrentMap(actualMap, sizeof(actualMap));
+
+	StrCat(demoName, 256, actualTime);
+	StrCat(demoName, 256, "_");
+	StrCat(demoName, 256, actualMap[3]);
+
+	PrintToChat(client, "Demo will be saved at gotv/%s.dem", demoName);
+
+	ServerCommand("tv_record gotv/%s", demoName);*/
+
+}
+
+CON_COMMAND_CHAT(stoprecord, "Stop demo recording")
+{
+	if (!player)
+		return;
+
+	int iCommandPlayer = player->GetPlayerSlot();
+
+	ZEPlayer *pPlayer = g_playerManager->GetPlayer(player->GetPlayerSlot());
+
+	if (!pPlayer->IsAdminFlagSet(ADMFLAG_SLAY))
+	{
+		ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIX "You don't have access to this command.");
+		return;
+	}
+
+	char buf[256];
+	char command[256];
+	
+	V_snprintf(buf, MAX_PATH, "Demo saved at gotv/%s", demoName);
+	ClientPrint(player, HUD_PRINTTALK, buf);
+
+	V_snprintf(command, MAX_PATH, "tv_stoprecord");
+	g_pEngineServer2->ServerCommand(command);
+	
 }
 
 CON_COMMAND_CHAT(ban, "ban a player")
@@ -309,15 +504,15 @@ CON_COMMAND_CHAT(map, "change map")
 		ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIX"Usage: !map <mapname>");
 		return;
 	}
-
+/*
 	if (!g_pEngineServer2->IsMapValid(args[1]))
 	{
 		ClientPrint(player, HUD_PRINTTALK, CHAT_PREFIX"Invalid map specified.");
 		return;
 	}
-
+*/
 	char buf[MAX_PATH];
-	V_snprintf(buf, sizeof(buf), "changelevel %s", args[1]);
+	V_snprintf(buf, sizeof(buf), "changelevel de_%s", args[1]);
 	g_pEngineServer2->ServerCommand(buf);
 }
 
@@ -327,9 +522,9 @@ void CAdminSystem::LoadAdmins()
 	KeyValues* pKV = new KeyValues("admins");
 	KeyValues::AutoDelete autoDelete(pKV);
 
-	if (!pKV->LoadFromFile(g_pFullFileSystem, "addons/cs2fixes/configs/admins.cfg"))
+	if (!pKV->LoadFromFile(g_pFullFileSystem, "addons/cs2scrim/configs/admins.cfg"))
 	{
-		Warning("Failed to load addons/cs2fixes/configs/admins.cfg\n");
+		Warning("Failed to load addons/cs2scrim/configs/admins.cfg\n");
 		return;
 	}
 	for (KeyValues* pKey = pKV->GetFirstSubKey(); pKey; pKey = pKey->GetNextKey())
@@ -367,9 +562,9 @@ void CAdminSystem::LoadInfractions()
 	KeyValues* pKV = new KeyValues("infractions");
 	KeyValues::AutoDelete autoDelete(pKV);
 
-	if (!pKV->LoadFromFile(g_pFullFileSystem, "addons/cs2fixes/data/infractions.txt"))
+	if (!pKV->LoadFromFile(g_pFullFileSystem, "addons/cs2scrim/data/infractions.txt"))
 	{
-		Warning("Failed to load addons/cs2fixes/data/infractions.txt\n");
+		Warning("Failed to load addons/cs2scrim/data/infractions.txt\n");
 		return;
 	}
 
@@ -431,7 +626,7 @@ void CAdminSystem::SaveInfractions()
 		pKV->AddSubKey(pSubKey);
 	}
 
-	pKV->SaveToFile(g_pFullFileSystem, "addons/cs2fixes/data/infractions.txt");
+	pKV->SaveToFile(g_pFullFileSystem, "addons/cs2scrim/data/infractions.txt");
 }
 
 void CAdminSystem::AddInfraction(CInfractionBase* infraction)
