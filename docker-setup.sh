6da463bc35ab3c42237771034b00e229d82753d5
marqdevx <marqdevx@users.noreply.github.com>
2023-12-30 14:26:48 +0100

    Fix build script

--- docker-setup.sh
+++ docker-setup.sh
@@ -25,7 +25,8 @@ cd "/dockerVolume/mm-cs2-scrim"
 mkdir build
 cd build
 
-python ../configure.py --enable-optimize --symbol-files --sdks cs2
+#python ../configure.py --enable-optimize --symbol-files --sdks cs2
+python3 ../configure.py --sdks cs2 --targets x86_64
 ambuild
 
 
